a32962f3ea926e3cc7255cad0a3ed00f9cf19619
Luke <luket8032@hotmail.com>
2023-09-09 13:56:21 -0500

    Clean up code and add smooth scrolling

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const test = document.getElementById('test');
 
 function sendEmail(e) {
     e.preventDefault();
-    console.log('pressed')
     const params = {
         user_name: document.getElementById('name').value ,
         user_email: document.getElementById('email').value ,
         message: document.getElementById('message').value
-    }
+    };
     emailjs.send('service_jmfetmf', 'template_w3farno', params, 'RnVT1zlkdz8VevDyP')
     .then((res) => {
         document.getElementById('name').value = "";
@@ -18,7 +17,4 @@ function sendEmail(e) {
     })
 }
 
-// contactForm.addEventListener('click', sendEmail);
-
-test.addEventListener('click', sendEmail)
-console.log('working')
+test.addEventListener('click', sendEmail);
